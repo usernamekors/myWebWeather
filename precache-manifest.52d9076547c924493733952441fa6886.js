53d0ec39db0c46dc4f31b2abbbb3361678ff3336
robak <marek.kors@wp.pl>
2019-03-24 15:46:27 +0100

    Updates

---
+++ precache-manifest.52d9076547c924493733952441fa6886.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a13aac9891e477293ae",
+    "url": "/myWebWeather/static/js/runtime~main.8ab31626.js"
+  },
+  {
+    "revision": "6e11e8e2ed087df39f62",
+    "url": "/myWebWeather/static/js/main.f36210b7.chunk.js"
+  },
+  {
+    "revision": "6eb2baa501d3f666aae7",
+    "url": "/myWebWeather/static/js/2.d1794fd5.chunk.js"
+  },
+  {
+    "revision": "6e11e8e2ed087df39f62",
+    "url": "/myWebWeather/static/css/main.d8dabc7f.chunk.css"
+  },
+  {
+    "revision": "eca71bdfc6a76c0b60a5e75ea66ba47a",
+    "url": "/myWebWeather/index.html"
+  }
+];
